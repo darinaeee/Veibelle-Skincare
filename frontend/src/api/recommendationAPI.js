86ca83c0fca6d5473aa846ac13e308bdc586b557
darlina <darinaeee0@gmail.com>
2025-11-07 11:07:55 +0800

    Update frontend and backend latest code for deployment

--- frontend/src/api/recommendationAPI.js
+++ frontend/src/api/recommendationAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://skincare-withered-glitter-1420.fly.dev";
 
 export async function getRecommendations({ skin_type, allergens_list }) {
   const params = new URLSearchParams();
